8b86d153308d98369436317ac5d1cf1435f93202
Wolfie <Wolfie@wolfietech.ca>
2017-04-26 15:11:06 -0700

    Version 2 Pretty much a rewrite
    Rewrote the body and css theming and changed site logos to a uniform svg

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,7 @@
 	<meta name="google-site-verification" content="6TdKz-Qjz2dsDNG-nfOLF-F8arC418nI_liyZRAX_U4" />
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<!-- Material Design Lite -->
-	<!-- Broken <?php include_once("mdlite.php") ?> -->
+	<?php include_once("mdlite.php") ?>
 	<title>Wolfietech Site Selection</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<!-- Facebook/Open Graph Tags -->
@@ -28,26 +28,49 @@
 	<meta name="theme-color" content="#ffffff">
 	<!-- Google Analytics Tracking -->
 	<?php include_once("analyticstracking.php") ?>
-	</head>
-  <body>
-    <div class="page">
-      <div id="selections">
-        <table>
-          <tr>
-            <th colspan=2 style="height:12px;">Select a Area to Continue</th>
-          </tr>
-		      <tr>
-            <th><div id="sitepic"><a href="https://dev.wolfietech.ca"><img src="./media/coding.jpg" height="100%" width="100%"></a></div></th>
-            <th><div id="sitepic"><a href="https://blog.wolfietech.ca"><img src="./media/blog.png" height="100%" width="100%"></a></div></th>
-          </tr>
-		      <tr id="text">
-            <td><a href="https://dev.wolfietech.ca">Developing Corner<br>dev.wolfietech.ca</a></td>
-            <td><a href="https://blog.wolfietech.ca">Blogging Corner<br>blog.wolfietech.ca</a></td>
-          </tr>
-        </table>
+</head>
+<body>
+  <div class="flex-container">
+    <header>
+      <div id="headerbar"><img id="logo" src="./media/1201x1201.png" height="100" width="100"></img><div id="headertitle_text">WolfieTech Site&nbsp;Selection</div></div>
+    </header>
+    <div class="selectionContainer">
+      <div id="developing_div">
+        <!-- <img src="./media/coding.png" height="100" width="100"></img>-->
+        <a href="https://dev.wolfietech.ca/" id="developing_link">
+          <object id="dev_logo" type="image/svg+xml" data="./media/dev.svg" >
+            <param name="src" value="./media/dev.svg">
+          </object>
+          <h4>Developing</h4>
+        </a>
+      </div>
+      <div id="blog_div">
+        <!-- <img src="./media/blog.png" height="100" width="100"></img> -->
+        <a href="https://blog.wolfietech.ca/" id="blog_link">
+          <object id="blog_logo" type="image/svg+xml" data="./media/blog.svg" >
+            <param name="src" value="./media/blog.svg">
+          </object>
+          <h4>Blog</h4>
+        </a>
+      </div>
+      <div id="uploads_div">
+        <!-- <img src="./media/uploads.png" height="100" width="100"></img>-->
+        <a href="https://uploads.wolfietech.ca/" id="uploads_link">
+          <object id="uploads_logo" type="image/svg+xml" data="./media/uploads.svg" >
+            <param name="src" value="./media/uploads.svg">
+          </object>
+          <h4>Uploads</h4>
+        </a>
       </div>
     </div>
-  </body>
-  <footer>
-  </footer>
+    <footer>
+      Contact Me: <script>document.write('<'+'a'+' '+'h'+'r'+'e'+'f'+'='+"'"+'&'+'#'+'1'+'0'+'9'+';'+'&'+'#'+'9'+'7'+';'+'i'+'l'+'&'+'#'+'1'+
+'1'+'6'+';'+'o'+'&'+'#'+'5'+'8'+';'+'&'+'#'+'1'+'1'+'9'+';'+'%'+'&'+'#'+'5'+'4'+';'+'F'+'l'+'f'+'i'+
+'e'+'&'+'#'+'6'+'4'+';'+'w'+'&'+'#'+'1'+'1'+'1'+';'+'l'+'%'+'6'+'6'+'%'+'6'+'&'+'#'+'5'+'7'+';'+'e'+
+'&'+'#'+'1'+'1'+'6'+';'+'e'+'c'+'h'+'&'+'#'+'4'+'6'+';'+'&'+'#'+'9'+'9'+';'+'a'+"'"+'>'+'&'+'#'+'1'+
+'1'+'9'+';'+'o'+'l'+'f'+'i'+'&'+'#'+'1'+'0'+'1'+';'+'&'+'#'+'6'+'4'+';'+'w'+'&'+'#'+'1'+'1'+'1'+';'+
+'l'+'f'+'&'+'#'+'1'+'0'+'5'+';'+'e'+'t'+'e'+'&'+'#'+'9'+'9'+';'+'h'+'&'+'#'+'4'+'6'+';'+'c'+'a'+'<'+
+'/'+'a'+'>');</script><noscript>[Turn on JavaScript to see the email address]</noscript>
+    </footer>
+</body>
 </html>
