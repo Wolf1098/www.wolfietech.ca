879ada8d3076a882ced1afc4939c121016d3d410
Wolfie <Wolfie@wolfietech.ca>
2017-04-26 15:08:22 -0700

    Version 1
    Version 1 of www.wolfietech.ca

---
+++ htdocs/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<meta name="google-site-verification" content="6TdKz-Qjz2dsDNG-nfOLF-F8arC418nI_liyZRAX_U4" />
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Material Design Lite -->
+	<!-- Broken <?php include_once("mdlite.php") ?> -->
+	<title>Wolfietech Site Selection</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- Facebook/Open Graph Tags -->
+	<meta property="og:title" content="WolfieTech Site Section Screen" />
+	<meta property="og:url" content="https:&#47;&#47;www.wolfietech.ca" />
+	<meta property="og:description" content="Welcome to WolfieTech, Here you can choose what site of mine you want to browse." />
+	<meta property="og:image" content="https:&#47;&#47;www.wolfietech.ca/media/1201x1201.png" />
+	<meta property="fb:admins" content="1322233847" />
+	<!-- Favicon Settings -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png?v=2" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png?v=2" sizes="16x16">
+	<link rel="manifest" href="/manifest.json?v=2">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#0085ab">
+	<link rel="shortcut icon" href="/favicon.ico?v=2">
+	<meta name="apple-mobile-web-app-title" content="WolfieTech">
+	<meta name="application-name" content="WolfieTech">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="msapplication-TileImage" content="/mstile-144x144.png?v=2">
+	<meta name="theme-color" content="#ffffff">
+	<!-- Google Analytics Tracking -->
+	<?php include_once("analyticstracking.php") ?>
+	</head>
+  <body>
+    <div class="page">
+      <div id="selections">
+        <table>
+          <tr>
+            <th colspan=2 style="height:12px;">Select a Area to Continue</th>
+          </tr>
+		      <tr>
+            <th><div id="sitepic"><a href="https://dev.wolfietech.ca"><img src="./media/coding.jpg" height="100%" width="100%"></a></div></th>
+            <th><div id="sitepic"><a href="https://blog.wolfietech.ca"><img src="./media/blog.png" height="100%" width="100%"></a></div></th>
+          </tr>
+		      <tr id="text">
+            <td><a href="https://dev.wolfietech.ca">Developing Corner<br>dev.wolfietech.ca</a></td>
+            <td><a href="https://blog.wolfietech.ca">Blogging Corner<br>blog.wolfietech.ca</a></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </body>
+  <footer>
+  </footer>
+</html>
